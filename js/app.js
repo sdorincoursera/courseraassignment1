3cf69c84d42c141c1c824f46753c921a30d37007
dorin stan <dorin.stan@gmail.com>
2017-05-11 23:56:09 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function(){
+    'strict mode';
+    angular.module('LunchCheck',[])
+        .controller('LunchCheckController',myController);
+    
+    myController.$inject=['$scope','$filter'];
+    
+    function myController($scope,$filter){
+        
+    }
+    
+}();

82864d796a340c234df43c9325880f0bf8f49bc4
dorin stan <dorin.stan@gmail.com>
2017-05-12 01:14:32 +0300

    almost there

--- js/app.js
+++ js/app.js
@@ -3,10 +3,33 @@
     angular.module('LunchCheck',[])
         .controller('LunchCheckController',myController);
     
-    myController.$inject=['$scope','$filter'];
+    myController.$inject=['$scope'];
     
-    function myController($scope,$filter){
+    function myController($scope){
+        $scope.myArray="";
+        $scope.messageStyle="";
         
+        $scope.checkFood=function(){
+            if($scope.myArray==""){$scope.myMessage="Please enter data first";} else {
+                var arr=$scope.myArray.split(",");
+                var message="";
+                var checkIfEmpty=0;
+                
+
+                for(var i=0;i<arr.length;i++){
+                    console.log("item "+i+":"+arr[i]);
+                    if(arr[i]==""){checkIfEmpty+=1;}
+                }
+                
+                if(arr.length-checkIfEmpty<=3){
+                    $scope.myMessage="Enjoy!";
+                    $scope.messageStyle="color:green";
+                }else{
+                    $scope.myMessage="Too much!"
+                    $scope.messageStyle="color:red";
+                }
+            }
+        }
     }
     
-}();
+})();
